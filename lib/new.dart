0cacc196a4e845d9ff87b0ea0218aba0bc67f778
MalikAbdulSattar <abdulsattarabdulsattar904@gmail.com>
2024-01-06 22:46:03 +0500

    by sattar

---
+++ lib/new.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class Editing extends StatefulWidget {
+  const Editing({super.key});
+
+  @override
+  State<Editing> createState() => _EditingState();
+}
+
+class _EditingState extends State<Editing> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.3),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: Alignment(2, 9),
+                child: Text(
+                  'Edit',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w600,
+                    height: 0,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'View',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w600,
+                  height: 0,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Creat Folder',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w600,
+                  height: 0,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Recycle Bin',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w600,
+                  height: 0,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Setting',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w600,
+                  height: 0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
