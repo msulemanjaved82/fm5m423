8af1ffadf72ab7eff6c6c88b72acef5e1bdc269a
Hammadyousaf1 <hammadyosuf1@gmail.com>
2024-01-06 15:22:51 +0500

    mhy

--- lib/Image folder screen.dart
+++ lib/Image folder screen.dart
@@ -1,5 +1,7 @@
-import 'package:filemanager/Home.dart';
+import 'package:file_management_app/Home.dart';
+import 'package:file_management_app/sorting%20menu.dart';
 import 'package:flutter/material.dart';
+import 'package:popover/popover.dart';
 
 class Imagefolder extends StatelessWidget {
   const Imagefolder({super.key});
@@ -117,23 +119,7 @@ class Imagefolder extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Icon(
-                      Icons.sort,
-                      color: Colors.white,
-                    ),
-                    SizedBox(
-                      width: 4,
-                    ),
-                    Text(
-                      'Date',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w500,
-                        height: 0,
-                      ),
-                    ),
+                    sortingbutton(),
                   ],
                 ),
                 SizedBox(
@@ -903,6 +889,44 @@ class Imagefolder extends StatelessWidget {
               ],
             ),
           )),
+    );
+  }
+}
+
+class sortingbutton extends StatelessWidget {
+  const sortingbutton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => showPopover(
+          context: context,
+          bodyBuilder: (context) => menuitems(),
+          height: 105,
+          width: 100,
+          direction: PopoverDirection.bottom),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Icon(
+            Icons.sort,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 4,
+          ),
+          Text(
+            'Date',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.w500,
+              height: 0,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
