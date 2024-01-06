0993122f93fe7d45249e5871ea512da1237b0562
Hammadyousaf1 <hammadyosuf1@gmail.com>
2024-01-06 15:02:33 +0500

    by hammad

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:filemanager/Home.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp1());
+}
+
+class MyApp1 extends StatefulWidget {
+  const MyApp1({super.key});
+
+  @override
+  State<MyApp1> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp1> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+          // ···
+          ),
+      home: file_management_app(),
+    );
+  }
+}
